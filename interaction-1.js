f896fd212144dd3d3193b809cc9f7d84c15baf9f
Eli <correu.elio@gmail.com>
2025-11-19 14:08:38 +0100

    test9 int1

--- interaction-1.js
+++ interaction-1.js
@@ -54,9 +54,9 @@ let oldmovetimer = 0;
 
 function accelerationChange(accx, accy, accz) {
     movetimer = millis();
-    if (((accx < 3 && accx > 0.2) || (accx > -3 && accx < -0.2)) &&
-        ((accy < 3 && accy > 0.2) || (accy > -3 && accy < -0.2)) &&
-        ((accz < 3 && accz > 0.2) || (accz > -3 && accz < -0.2))) {
+    if (((accx < 3 && accx > 0.1) || (accx > -3 && accx < -0.1)) &&
+        ((accy < 3 && accy > 0.1) || (accy > -3 && accy < -0.1)) &&
+        ((accz < 3 && accz > 0.1) || (accz > -3 && accz < -0.1))) {
         if (movetimer - oldmovetimer > 250) {
             statusLabels[2].style("color", "pink");
             playAudio();
